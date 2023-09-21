23ca4ceb1827a634a77bd2e1ee723addedda27a5
Cameron Beattie <bsi017@usask.ca>
2023-09-21 11:01:43 -0600

    bare bones react and flask app???

--- flask-server/server.py
+++ flask-server/server.py
@@ -7,3 +7,26 @@ def members():
 
 if(__name__ == "__main__"):
     app.run(debug=True)
+
+
+# To run: cd into the flask-server directory
+
+# Set-ExecutionPolicy Unrestricted -Scope Process
+# (Allows you to run the script to start the venv)
+
+# .\venv\Scripts\activate
+# to actually start the venv
+
+# py server.py
+# to actually run the back end server
+
+
+#CTRL + c to stop the local server
+
+
+
+
+######### TO RUN THE FRONT END ##########
+
+# cd into react-app
+# npm run dev
