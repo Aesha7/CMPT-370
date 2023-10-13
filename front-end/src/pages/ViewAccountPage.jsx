21296caa2260eb3a76e9087a56ecade3318a0d42
Cameron Beattie <306skhoops@gmail.com>
2023-10-13 14:28:06 -0600

    viewaccount started

--- front-end/src/pages/ViewAccountPage.jsx
+++ front-end/src/pages/ViewAccountPage.jsx
@@ -1,13 +1,52 @@
 import React, { useState } from "react";
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 import { Route, Routes, useNavigate } from "react-router";
-import './ViewAccountPage.css';
-
+import "./ViewAccountPage.css";
 
 const AccountView = () => {
+  const [name] = useState("John Doe");
+  const [phone] = useState("(306) 123-4567");
+  const [email] = useState("abc123@domain.ca");
+  const [birthday] = useState("month/day/year");
+  const [password] = useState("");
 
-    return(<button className="settingsButton">&#x26ED;</button>);
-}
+  return (
+    <div className="view-account-page">
+      <div className="view-account-top-bar">
+        My Account
+        <button className="view-account-settingsButton">&#x26ED;</button>
+      </div>
 
-export default AccountView
+      <div className="view-account-container">
+        <div className="view-account-container-toprow">
+          <div className="view-account-column-entry">
+            <label className="view-account-label" for="email">
+              Email:
+            </label>
+            <input
+              className="text-field"
+              value={email}
+              type="email"
+              id="email"
+            />
+          </div>
+
+          <div className="view-account-column-entry">
+            <label className="view-account-label" for="password">
+              Password:
+            </label>
+            <input
+              className="view-account-text-field"
+              value={password}
+              type="password"
+              id="password"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountView;
