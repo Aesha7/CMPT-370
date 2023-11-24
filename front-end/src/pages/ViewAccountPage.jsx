089c38e36b2dc4657bacd2e6a29b90b534482657
Cameron Beattie <306skhoops@gmail.com>
2023-11-24 14:07:31 -0600

    date picker positioning (not done)
    got rid of placeholders

--- front-end/src/pages/ViewAccountPage.jsx
+++ front-end/src/pages/ViewAccountPage.jsx
@@ -11,10 +11,10 @@ const AccountView = () => {
   let renders;
 
   // use these variables to set proper data
-  let [name, setName] = useState("John Doe");
-  let [phone, setPhone] = useState("(306) 123-4567");
-  let [email, setEmail] = useState("email@domain.com");
-  let [birthday, setBirthday] = useState("month/day/year");
+  let [name, setName] = useState("");
+  let [phone, setPhone] = useState("");
+  let [email, setEmail] = useState("");
+  let [birthday, setBirthday] = useState("");
   let [userID, setUserID] = useState("");
   let [staffLevel, setStaffLevel] = useState("");
 
@@ -576,6 +576,7 @@ const AccountView = () => {
             </label>
             <DatePicker
               className="custom-datepicker-addFamily"
+              portalId="root-portal"
               showPopperArrow={false}
               selected={newBirthday}
               onChange={handleNewBirthday}
@@ -588,11 +589,13 @@ const AccountView = () => {
               dropdownMode="select"
               placeholderText="Select a date"
 
-              popperPlacement="top-start"
+              popperPlacement="right-end"
+              // popperPlacement="top"
+
               popperModifiers={{
                 offset: {
                   enabled: true,
-                  offset: [20, 20]
+                  offset: [5, 40]
                 },
                 preventOverflow: {
                   enabled: true,
